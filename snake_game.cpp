0128693b17b36169a860f86e1f88080bb644ebe2
cl3vy <116805095+cl3vy@users.noreply.github.com>
2022-11-05 14:11:37 +0100

    Add files via upload

---
+++ snake_game.cpp
@@ -0,0 +1,152 @@
+#include <iostream>
+#include <conio.h>
+#include <windows.h>
+using namespace std;
+bool game_over;
+const int width {20};
+const int height {20};
+int x, y, fruitX, fruitY, score;
+int tailX[100], tailY[100];
+int nTail;
+enum eDirection {STOP=0, LEFT, UP, RIGHT, DOWN};
+eDirection dir;
+
+void Setup(){
+    game_over = false;
+    dir = STOP;
+    x = width / 2;
+    y = height / 2;
+    fruitX = rand() % width;
+    fruitY = rand() % height;
+    score = 0;
+}
+
+void Draw(){
+    system("cls");
+    for (int i = 0; i < width + 2; i++)
+        cout << "#";
+    cout << endl;
+
+    for(int i = 0; i< height; i++){
+        for (int j = 0; j < width; j++){
+            if (j == 0)
+                cout << "#";
+            if (i == y && j == x)
+                cout << "0";
+            else if (i== fruitY && j == fruitX)
+                cout << "F";
+            else 
+            {
+                bool print = false;
+                for (int k = 0; k < nTail; k++){
+                    if (tailX[k] == j && tailY[k] == i){
+                        cout << "o";
+                        print = true;
+                    }
+                }
+                if (!print)
+                    cout << " "; 
+            }
+            
+            if (j == width-1)
+                cout << "#";
+        }
+        cout << endl;
+    }
+
+    for (int i = 0; i < width + 2; i++)
+        cout << "#";
+    cout << endl;
+
+    cout << "\n\n\n Score: " << score;
+}
+
+void Input(){
+    if (_kbhit()){
+        switch(_getch()){
+            case 'a':
+                dir = LEFT;
+                break;
+            case 'd':
+                dir = RIGHT;
+                break;
+            case 's':
+                dir = DOWN;
+                break;
+            case 'w':
+                dir = UP;
+                break;
+            case 'x':
+                game_over = true;
+                break;               
+        }
+    }
+}
+void Logic(){
+
+    int prevX = tailX[0];
+    int prevY = tailY[0];
+    int prev2X, prev2Y;
+    tailX[0] = x;
+    tailY[0] = y;
+    
+    for (int i = 1; i < nTail; i++){
+        prev2X = tailX[i];
+        prev2Y = tailY[i];
+        tailX[i] = prevX;
+        tailY[i] = prevY;
+        prevX= prev2X;
+        prevY = prev2Y;
+    }
+
+    switch (dir)
+    {
+        case LEFT:
+            x--;
+            break;
+        case RIGHT:
+            x++;
+            break;
+        case UP:
+            y--;
+            break;
+        case DOWN:
+            y++;
+            break;
+        default:
+            break;
+    }
+
+    if (x > width || x < 0 || y > height || y < 0)
+        game_over = true;
+   
+    for (int i = 0; i < nTail; i++ ){
+        if (tailX[i] == x && tailY[i] == y)
+            game_over = true;
+    }
+   
+    {
+        /* code */
+    }
+    
+
+    if (x == fruitX && y == fruitY){
+        score += 10;
+        fruitX = rand() % width;
+        fruitY = rand() % height;
+        nTail++;
+    }
+}
+
+
+int main(){
+    
+    Setup();
+    while(!game_over){
+        Draw();
+        Input();
+        Logic();
+        Sleep(60);
+    }
+    return 0;
+}
